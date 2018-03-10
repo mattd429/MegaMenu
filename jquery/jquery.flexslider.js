1fb2fc4697635b5ff22434787cb18c6c5b33dde4
Matthew Brown <mattd429@gmail.com>
2018-03-09 19:19:52 -0500

    Update jquery.flexslider.js

--- jquery/jquery.flexslider.js
+++ jquery/jquery.flexslider.js
@@ -1,1 +1,19 @@
+/*
+ * jQuery FlexSlider v2.6.2
+ * Copyright 2012 WooThemes
+ * Contributing Author: Tyler Smith
+ */
+;
+(function ($) {
+  
+  var focused = true;
+  
+  //Flexsider: Object Instance
+  $.flexslider = function(el, options) {
+    var slider = $(el);
+    
+    // making variables public
+    slider.vars = $.extend({}, $.flexslider.defaults, options);
+  }
 
+})
