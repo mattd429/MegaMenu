27ca1aaad2a4d624aecd82b3c849ace707a31cfa
Matthew Brown <mattd429@gmail.com>
2018-04-04 19:01:34 -0400

    Update jquery-v2.1.0.js
    here we just write the simple factory to allow you to use "$" for jQuery in DOM environment, if we don't have some kind of DOM environment then we will throw an error and will not be able to use jQuery, this is most likely never going to be the case.

--- jquery/jquery-v2.1.0.js
+++ jquery/jquery-v2.1.0.js
@@ -1,1 +1,36 @@
+/*!
+ * jQuery JavaScript Library v2.1.0
+ * http://jquery.com/
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ *
+ * Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2014-01-23T21:10Z
+ */
 
+(function( global, factory ) {
+
+  if ( typeof module === "object" && typeof module.exports === "object" ) {
+      // For CommonJS and CommonJS-Like environments where a proper window is present,
+      // execute the factory($) and get jQuery
+      // For environments tat do not inherently posses a window with a document
+      // (such as Node.js), expose a jQuery-making factory as module.exports
+      // This accentuates the need for the creation of a real window
+      // e.g. var jQuery = require("jquery")(window);
+      // See ticket #14549 for more infor
+      module.exports = global.document ?
+          factory( global, true ) :
+          function ( w ) {
+              if ( !w.document ) {
+                  throw new Error("Jquery requies a window with a document" );
+              }
+              return factory( w );
+        };
+  } else {
+      factory( global );
+  } 
+})
