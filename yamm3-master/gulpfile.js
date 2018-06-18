10576d4833c3e753ac64fbe6ac41f5f68892fef5
Matthew Brown <mattd429@gmail.com>
2018-06-17 21:24:21 -0400

    Update gulpfile.js
    changed requirement from gulp-jade to gulp-pug since we will use that instead.

--- yamm3-master/gulpfile.js
+++ yamm3-master/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp      = require('gulp'),
-    jade      = require('gulp-jade'),
+    jade      = require('gulp-pug'),
     less      = require('gulp-less'),
     path      = require('path'),
     marked    = require('marked'), // For :markdown filter in jade
