c836ec78c78dbfb85661cfbf51a543ddbdefd229
Matthew Brown <mattd429@gmail.com>
2018-06-18 10:30:10 -0400

    Update gulpfile.js

--- yamm3-master/gulpfile.js
+++ yamm3-master/gulpfile.js
@@ -40,3 +40,61 @@ function handleError(err) {
 //---------------
 // TASKS
 //---------------
+
+// Compile LESS into a path described and handle any errors if there are errors.
+gulp.task('yamm', function() {
+    return gulp.src(source.styles.app.main)
+        .pipe(less({
+            paths: [source.styles.app.dir]
+    }))
+    .on("error", handleError)//handle our error function
+    .pipe(gulp.dest(build.styles));
+});
+
+//PUG compile our pug templates and make it clean it up, display errors w/ handleError mfunc
+//pipe in htmlhint and store in build dist folder.
+gulp.task('templates', function() {
+    return gulp.src(source.templates.app.files)
+        .pipe(pug({
+            pretty: true
+    }))
+    .on("error", handleError)
+    .pipe(htmlhint())// validate our pug templates
+    .pipe(htmlhint.reporter())//report our validation
+    .pipe(gulp.dest(build.templates.app)); // build in dist folder
+});
+
+
+//---------------
+// WATCH
+//---------------
+
+// Watch for file changes
+gulp.task('watch:demo', function() {
+    gulp.watch(source.tempaltes.app.watch,      ['templates']);//watch pug templates for changes
+});
+
+gulp.task('watch:yamm', function() {
+    gulp.watch(source.styles.app.watch,     ['yamm']);//watch changes in the yamm dir
+});
+
+
+//---------------
+// DEFAULT TASK
+//---------------
+
+gulp.task('default', [
+          'yamm',
+          'watch:yamm'
+        ]);
+
+//---------------
+// DEMO TASK
+//---------------
+
+gulp.task('demo', [
+          'yamm',
+          'templates',
+          'watch:yamm',
+          'watch:demo'
+        ]);
