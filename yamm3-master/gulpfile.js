61b5e3e25dfe685e1e4806879ecedaa89d084041
Matthew Brown <mattd429@gmail.com>
2018-06-17 21:23:12 -0400

    Update gulpfile.js
    added requirements for gulp, gulp is before webpack, minifies our code watch out index.pug and yams.less files for changes, build our gulp inside of yamm dir, produce an error function on any errors.

--- yamm3-master/gulpfile.js
+++ yamm3-master/gulpfile.js
@@ -1,1 +1,42 @@
+var gulp      = require('gulp'),
+    jade      = require('gulp-jade'),
+    less      = require('gulp-less'),
+    path      = require('path'),
+    marked    = require('marked'), // For :markdown filter in jade
+    htmlhint  = require('gulp-htmlhint');
 
+
+// SOURCES CONFIG
+var source = {
+  templates: {
+    app: {
+      files: ['demo/pug/index.pug'],
+      watch: ['demo/pug/*.pug']
+    }
+  },
+  styles: {
+    app: {
+      main: ['yamm/yamm.less'],
+      dir: 'yamm',
+      watch: ['yamm/yamm.less']
+    }
+  }
+};
+
+// BUILD TARGET CONFIG
+var build = {
+  styles: './yamm',
+  templats: {
+    app: './'
+  }
+};
+
+// ERROR HANDLER
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+//---------------
+// TASKS
+//---------------
